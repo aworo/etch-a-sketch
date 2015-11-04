da9e004cc933eb8ef113f699ec8fd37aa7574d32
aworo <ajworoniuk@gmail.com>
2015-11-03 19:27:40 -0800

    grid of divs created

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  for(i = 0; i < 16; i++) {
-    $('body').append('<div id="div'+ i +'" />');
-  }
+   /* for(i = 0; i < 16; i++) {
+  	    for(j = 0; j < 16; j++) {
+  		    $('#grid').append('<div id="div'+ i +'" class="box"/>');
+    	}
+*/
+    var container = $(".container").html("");
+	for (var row = 0; row < 16; row++) {
+		for (var column = 0; column < 16; column++) {
+			container.append( $("<div class='grid'></div>"));
+		}
+	
+	}
+
+    
 });
+
